d4fb7ab2b4bdf4328094a6b4e9edfa4d903b29bb
Yanyuan Qin <yanyuan.qin@amd.com>
2026-07-28 14:46:16 +0000

    perf(flydsl): fuse Kimi-K3 B1 latent MoE tail

--- aiter/ops/flydsl/__init__.py
+++ aiter/ops/flydsl/__init__.py
@@ -38,6 +38,7 @@ if is_flydsl_available():
         )
 
     from .gemm_kernels import flydsl_hgemm, flydsl_preshuffle_gemm_a8
+    from .latent_moe_tail import latent_moe_tail, supports_latent_moe_tail
     from .moe_kernels import flydsl_moe_stage1, flydsl_moe_stage2
     from .fmha_kernels import flydsl_flash_attn_func
     from .kimi_k3_kda_decode import (
@@ -65,6 +66,8 @@ if is_flydsl_available():
         "flydsl_moe_stage1",
         "flydsl_moe_stage2",
         "flydsl_hgemm",
+        "latent_moe_tail",
+        "supports_latent_moe_tail",
         "flydsl_flash_attn_func",
         "flydsl_kimi_k3_kda_decode",
         "flydsl_kimi_k3_kda_decode_with_f_b",
